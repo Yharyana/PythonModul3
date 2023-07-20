d566959ab63b15f3a5a527a95510edc265ea8900
Jakub Zbroiński <kubazbroinsk@gmail.com>
2023-07-20 13:21:39 +0200

    dziala

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-i = 1
-while i < 6:
-  print(i)
-  i += 1
-lista_sports=["triatrlon","bobslej","bridge","soccer"]
-index_sports=0
-while index_sports<len(lista_sports):
-    if index_sports % 2>0:
-        print(lista_sports[index_sports])
-    index_sports+=1
+#i = 1
+#while i < 6:
+#  print(i)
+ # i += 1
+#ista_sports=["triatrlon","bobslej","bridge","soccer"]
+#index_sports=0
+#while index_sports<len(lista_sports):
+#    if index_sports % 2>0:
+#        print(lista_sports[index_sports])
+#    index_sports+=1
 
-lista_nums=[2137,69,420,17]
-num_sum=0
-num_index=0
-while num_index<len(lista_nums):
-    num_sum+=lista_nums[num_index]
-    num_index+=1
+#lista_nums=[2137,69,420,17]
+#num_sum=0
+#num_index=0
+#while num_index<len(lista_nums):
+#    num_sum+=lista_nums[num_index]
+#    num_index+=1
 
-print(num_sum)
+#print(num_sum)
 
 #zadania
 
@@ -37,13 +37,86 @@ print(num_sum)
 #
 #print(formatted_celli)
 
-num=0
-proby=0
-while num % 2!=0 and proby<10 or num == 0 and proby<10:
-    proby += 1
-    num = int(input("Podaj liczbe parzysta wiekszą od 0. masz 10 prób"))
-if(proby<10):
-    print("udało ci sie")
-else:
-    print(" r y searius?")
+#tez spoko zadanie na poczaktu łatwe sie wydaje a nie dokkocna
+#num=0
+#proby=0
+#while num % 2!=0 and proby<10 or num == 0 and proby<10:
+#    proby += 1
+#    num = int(input("Podaj liczbe parzysta wiekszą od 0. masz 10 prób"))
+#if(proby<10):
+#    print("udało ci sie")
+#else:
+#    print(" r y searius?")
+
+#przekombinował w  i nie dizała edit: wystaczyło uzyc spolita
+#dania=input("podaj swoje uluione dania odzielajac  je przeicnkiem")
+#liczba_dots=0
+#liczba_dan=0
+#while liczba_dots<len(dania):
+#    if dania[liczba_dots]==",":
+#        liczba_dan+=1
+#        liczba_dots+=1
+#    else:
+#        liczba_dots+=1
+#liczba_dan+=1
+#print(liczba_dan)
+#lista_dan=[0]*liczba_dan
+#liczba_dots=0
+
+#pętla for
+
+#count=1
+#seasons=["wiosna","lato","jesien","zima"]
+#for season in seasons:
+#    print(count,season)
+#    count+=1
+#seasons=["wiosna","lato","jesien","zima"]
+#for count,season in enumerate(seasons,start=1):
+#    print(count,season)
+
+#zadania!
+
+# spoko zadanie całkiem
+#x=True
+#oceny=""
+#ocena_int=0;
+#srednia=0
+#suma=0;
+#ilosc=0
+#while x==True:
+#    y=input("Podaj ocene lub jeśli skonczyłes  napisz X")
+#    if y=="X":
+#        x=False
+#    else:
+#        oceny+=y
+#        ilosc+=1
+#
+#for i in oceny:
+#    ocena_int=int(i)
+#    suma+=ocena_int
+#    print(i)
+#srednia=suma/ilosc
+#print("oto twoja srednia ",srednia)
+
+
+#tel_num=input("Podaj swój numer telefonu")
+#tel_num=tel_num.replace("-","").replace(" ","")
+#formatted_celli=""
+
+#for index_celli,number in enumerate(tel_num):
+#    if index_celli % 3==0 and index_celli!=0:
+#        formatted_celli+=" "
+#        formatted_celli += number
+
+#    else:
+#        formatted_celli+=number
+
+
+#print(formatted_celli)
+
+
+
+
+
+
 
